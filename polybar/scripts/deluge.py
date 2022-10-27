06022fb1377a6858ab30af1de9d8af9616522e42
Martin Cation <martincation@protonmail.com>
2022-10-27 23:32:06 +0700

    polybar: precise torrent ratio

--- polybar/scripts/deluge.py
+++ polybar/scripts/deluge.py
@@ -5,15 +5,6 @@ def printExit(*args):
 	print(*args)
 	exit()
 
-def conciseRound(x):
-	l = int(math.log10(x))
-	s = str(x)
-	if (not "." in s): return s
-	dec = s.split(".")
-	dec[1] = dec[1][:max(0, 2-l)]
-	if (not len(dec[1])): return dec[0]
-	return ".".join(dec)
-
 def readConfig(where):
 	validKeys = ("host", "port", "user", "pass")
 	conf = {}
@@ -34,7 +25,7 @@ def getStatus(client):
 	try: status = client.call('core.get_torrents_status', {}, ["total_wanted", "total_uploaded"])
 	except FailedToReconnectException: printExit("")
 	except: printExit("Err")
-	printExit(conciseRound(100 * sum([status[i][b"total_uploaded"] for i in status])\
-	                           / sum([status[i][b"total_wanted"]   for i in status])))
+	printExit(round(100 * sum([status[i][b"total_uploaded"] for i in status])\
+	                           / sum([status[i][b"total_wanted"]   for i in status]), 2))
 
 getStatus(DelugeRPCClient(*readConfig(os.getenv("POLYBAR_ROOT") + "/.priv/deluge.conf")))
