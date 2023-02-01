13994ddcf7e905ee83d9b1bf1663ce42fb4e6b62
Martin Cation <martincation@protonmail.com>
2023-02-01 23:59:34 +0700

    neovim: dracula theme

--- nvim/lua/core/plugin_config/lualine.lua
+++ nvim/lua/core/plugin_config/lualine.lua
@@ -1,6 +1,7 @@
 require("lualine").setup {
 	options = {
 		icons_enabled = false,
+		theme = "dracula-nvim",
 		component_separators = { left = '', right = ''},
 		section_separators   = { left = '', right = ''},
 	},
--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -35,6 +35,8 @@ return require("packer").startup(function(use)
 		}
 	}
 
+	use "Mofiqul/dracula.nvim" -- Temporary theme
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
