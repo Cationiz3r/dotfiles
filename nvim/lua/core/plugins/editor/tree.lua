a6399d14ecb7f4378412069a1a6944b9cfdc6642
Martin Cation <martincation@protonmail.com>
2023-02-02 21:19:47 +0700

    neovim: tree keymap

--- nvim/lua/core/plugins/editor/tree.lua
+++ nvim/lua/core/plugins/editor/tree.lua
@@ -1,7 +1,19 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local api = require("nvim-tree.api")
+local function on_attach(bufnr)
+	vim.keymap.set("n", "<Tab>", "<c-w>p", { buffer = bufnr })
+	vim.keymap.set("n", "e", function()
+		api.tree.expand_all()
+	end, { buffer = bufnr })
+	vim.keymap.set("n", "<s-e>", function()
+		api.tree.collapse_all()
+	end, { buffer = bufnr })
+end
+
 require("nvim-tree").setup({
+	on_attach = on_attach,
 	view = {
 		width = 30,
 	},
@@ -22,4 +34,5 @@ require("nvim-tree").setup({
 	}
 })
 
-vim.keymap.set("n", "t", ":NvimTreeFocus<CR>")
+vim.keymap.set("n", "t", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<Tab>", "<c-w>p")
