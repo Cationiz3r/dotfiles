e32d86de66b465209cf01e816da0e246587bdd6b
Martin Cation <martincation@protonmail.com>
2023-02-02 10:14:38 +0700

    neovim: improve bufferline

--- nvim/lua/core/plugin_config/bufferline.lua
+++ nvim/lua/core/plugin_config/bufferline.lua
@@ -3,14 +3,41 @@ vim.opt.termguicolors = true
 require("bufferline").setup {
 	options = {
 		middle_mouse_command = "bdelete! %d",
+		right_mouse_command = "vertical sbuffer %d",
+
 		offsets = {{
 			filetype = "NvimTree",
 			text = "Tree",
 			text_align = "center",
 			separator = true
 		}},
+
+		max_name_length = 18,
+		tab_size = 0,
+		show_buffer_icons = false, -- on tab buttons
+		show_buffer_close_icons = false, -- on tab buttons
+		show_close_icon = false, -- end of bar
+
+		separator_style = { "", "" },
 	}
 }
 
+vim.api.nvim_set_hl(0, "BufferLineBackground", { bg = "#191a21" })
+vim.api.nvim_set_hl(0, "BufferLineBufferSelected", { bold = true })
+vim.api.nvim_set_hl(0, "BufferLineBufferVisible", { link = "BufferLineBufferSelected" })
+vim.api.nvim_set_hl(0, "BufferLineOffsetSeparator", { link = "VertSplit" })
+vim.api.nvim_set_hl(0, "BufferLineSeparator", { link = "VertSplit" })
+vim.api.nvim_set_hl(0, "BufferLineSeparatorSelected", { link = "VertSplit" })
+vim.api.nvim_set_hl(0, "BufferLineSeparatorVisible", { link = "VertSplit" })
+
 vim.keymap.set("n", "<c-w>", ":bp<bar>sp<bar>bn<bar>bd<CR>")
-vim.keymap.set("n", "<Space><Tab>", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<silent><leader><Tab>", "<Cmd>BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<silent><leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>")
+vim.keymap.set("n", "<silent><leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>")
+vim.keymap.set("n", "<silent><leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>")
+vim.keymap.set("n", "<silent><leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>")
+vim.keymap.set("n", "<silent><leader>5", "<Cmd>BufferLineGoToBuffer 5<CR>")
+vim.keymap.set("n", "<silent><leader>6", "<Cmd>BufferLineGoToBuffer 6<CR>")
+vim.keymap.set("n", "<silent><leader>7", "<Cmd>BufferLineGoToBuffer 7<CR>")
+vim.keymap.set("n", "<silent><leader>8", "<Cmd>BufferLineGoToBuffer 8<CR>")
+vim.keymap.set("n", "<silent><leader>9", "<Cmd>BufferLineGoToBuffer 9<CR>")
