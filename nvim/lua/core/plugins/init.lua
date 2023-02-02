695fe239d223dc2937a46e64c42ca1ce0ed38bc6
Martin Cation <martincation@protonmail.com>
2023-02-02 14:26:11 +0700

    neovim: group editor-related plugins

--- nvim/lua/core/plugins/init.lua
+++ nvim/lua/core/plugins/init.lua
@@ -1,17 +1,4 @@
--- Packer
 require("core.plugins.packer")
-
--- Editor
-require("core.plugins.bufferline")
-require("core.plugins.colorscheme")
-require("core.plugins.comment")
-require("core.plugins.gitsigns")
-require("core.plugins.lualine")
-require("core.plugins.syntax")
-require("core.plugins.tree")
-
--- LSP
+require("core.plugins.editor")
 require("core.plugins.lsp")
-
--- Utilities
 require("core.plugins.telescope")
