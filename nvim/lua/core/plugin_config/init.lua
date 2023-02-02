3106bc24c8510fdeb862d68e12481e054d6e3ce3
Martin Cation <martincation@protonmail.com>
2023-02-02 13:28:43 +0700

    neovim: refactor structures

--- nvim/lua/core/plugin_config/init.lua
+++
@@ -1,8 +0,0 @@
-require("core.plugin_config.lualine")
-require("core.plugin_config.tree")
-require("core.plugin_config.treesitter")
-require("core.plugin_config.telescope")
-require("core.plugin_config.bufferline")
-require("core.plugin_config.dracula")
-require("core.plugin_config.comment")
-require("core.plugin_config.gitsigns")
---
+++ nvim/lua/core/plugins/init.lua
@@ -0,0 +1,14 @@
+-- Packer
+require("core.plugins.packer")
+
+-- Editor
+require("core.plugins.bufferline")
+require("core.plugins.colorscheme")
+require("core.plugins.comment")
+require("core.plugins.gitsigns")
+require("core.plugins.lualine")
+require("core.plugins.syntax")
+require("core.plugins.tree")
+
+-- Utilities
+require("core.plugins.telescope")
