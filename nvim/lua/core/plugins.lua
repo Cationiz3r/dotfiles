b6a56950690985a603e4649fca977335cf207666
Martin Cation <martincation@protonmail.com>
2023-02-02 13:10:29 +0700

    neovim: gitsigns

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -22,6 +22,7 @@ return require("packer").startup(function(use)
 		}
 	}
 	use "nvim-lualine/lualine.nvim"
+	use "lewis6991/gitsigns.nvim"
 	use "nvim-treesitter/nvim-treesitter"
 	use {
 		"nvim-telescope/telescope.nvim", tag = "0.1.1",
