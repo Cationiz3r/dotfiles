8b347526d445ee19072a3643413eff0ad8e4ba11
Martin Cation <martincation@protonmail.com>
2023-02-03 16:28:56 +0700

    neovim: gitsigns add/undo hunk with tree update

--- nvim/lua/core/plugins/editor/gitsigns.lua
+++ nvim/lua/core/plugins/editor/gitsigns.lua
@@ -1,1 +1,38 @@
-require('gitsigns').setup {}
+require("gitsigns").setup({
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+
+		-- Navigation
+		map("n", "]c", function()
+			if vim.wo.diff then return "]c" end
+			vim.schedule(function() gs.next_hunk() end)
+			return "<Ignore>"
+		end, {expr=true})
+
+		map("n", "[c", function()
+			if vim.wo.diff then return "[c" end
+			vim.schedule(function() gs.prev_hunk() end)
+			return "<Ignore>"
+		end, {expr=true})
+
+		-- Actions
+		map({ "n", "v" }, "gs", ":Gitsigns stage_hunk<CR>")
+		map({ "n", "v" }, "gr", ":Gitsigns reset_hunk<CR>")
+		map("n", "gS", gs.stage_buffer)
+		map("n", "gu", gs.undo_stage_hunk)
+		map("n", "gR", gs.reset_buffer)
+		map("n", "gp", gs.preview_hunk)
+	end
+})
+
+-- Update nvimtree
+vim.api.nvim_create_autocmd("User", {
+	pattern = "GitSignsUpdate",
+	callback = require("nvim-tree.api").tree.reload,
+})
--- nvim/lua/core/plugins/lsp/lspconfig.lua
+++ nvim/lua/core/plugins/lsp/lspconfig.lua
@@ -4,7 +4,7 @@ local on_attach = function(_, _)
 
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-	vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
+	vim.keymap.set("n", "gt", require("telescope.builtin").lsp_references, {})
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 end
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
