34cc579d81dc327be2ed0e3a93c21d7ec0b1ae81
Martin Cation <martincation@protonmail.com>
2023-03-01 01:58:33 +0700

    neovim: change lua lang server name

--- nvim/lua/core/plugins/lsp/mason_lspconfig.lua
+++ nvim/lua/core/plugins/lsp/mason_lspconfig.lua
@@ -1,7 +1,7 @@
 require("mason-lspconfig").setup({
 	ensure_installed = {
 		"clangd", -- C, C++
-		"sumneko_lua", -- Lua
+		"lua_ls", -- Lua
 		"jedi_language_server", -- Python
 		"lemminx", -- XML
 	},
