fd134b529e2cb9f2bff1281fe2a2528795b953d8
Martin Cation <martincation@protonmail.com>
2023-03-14 01:03:23 +0700

    zsh: move .cache to ~/.local

--- zsh/core/env/xdg.zsh
+++ zsh/core/env/xdg.zsh
@@ -5,7 +5,7 @@ export XDG_DATA_DIRS="/usr/local/share:/usr/share"
 export XDG_CONFIG_DIRS="/etc/xdg"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CACHE_HOME="$HOME/.local/cache"
 
 ## Applications
 export XAUTHORITY="$XDG_RUNTIME_DIR/xorg/auth"
