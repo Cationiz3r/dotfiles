f6b8808e9b28647570bad6f720da75b5d9fa2a0e
Martin Cation <martincation@protonmail.com>
2023-03-18 11:57:55 +0700

    neovim: fix some keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -4,13 +4,9 @@ vim.keymap.set("n", "q", ":q<CR>")
 vim.keymap.set("n", "<leader>d", ":qa!<CR>")
 vim.keymap.set("n", "<leader>q", ":wqa<CR>")
 vim.keymap.set("n", "<leader>w", ":wq<CR>")
-vim.keymap.set("n", "s", ":%s/\\s\\+$//e|noh|w<CR>")
+vim.keymap.set("n", "s", ":w<CR>")
 vim.keymap.set("n", "<leader>h", ":noh<CR>")
 vim.keymap.set("n", "r", ":source %<CR>")
-
--- Line operations
-vim.keymap.set("n", "j", "yy$p")
-vim.keymap.set("n", "k", "dd")
 
 -- Un/Redo
 vim.keymap.set("n", "z", ":silent undo<CR>")
