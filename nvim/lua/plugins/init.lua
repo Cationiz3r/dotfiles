882fa1d55bd3729b369bf37303adab97d92654d1
Martin Cation <martincation@protonmail.com>
2023-03-18 12:16:58 +0700

    neovim: rename auto-completion module

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 require("plugins.packer")
 require("plugins.theme") -- Needs to load first
+require("plugins.auto_completion")
 require("plugins.components")
 require("plugins.editor")
 require("plugins.lsp")
-require("plugins.completion")
 require("plugins.telescope")
