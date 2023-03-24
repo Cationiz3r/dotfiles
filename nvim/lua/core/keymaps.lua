426fc094a62a40fbde82e57d38a7f929421ff7ce
Martin Cation <martincation@protonmail.com>
2023-03-24 11:18:03 +0700

    neovim: U to redo

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -5,7 +5,4 @@ vim.keymap.set("n", "<leader>qq", ":wqa<CR>")
 vim.keymap.set("n", "s", ":w<CR>")
 vim.keymap.set("n", "<leader>h", ":noh<CR>")
 vim.keymap.set("n", "<leader>r", ":source %<CR>")
-
--- Un/Redo
-vim.keymap.set("n", "z", ":silent undo<CR>")
-vim.keymap.set("n", "Z", ":silent redo<CR>")
+vim.keymap.set("n", "U", "<c-r>")
