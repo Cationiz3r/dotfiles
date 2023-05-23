9cf6d2f902d364888f91be2e2ebb8e25d9c1b1a2
Martin Cation <martincation@protonmail.com>
2023-05-23 20:24:33 +0700

    packer: change mason section name

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -29,7 +29,7 @@ return require("packer").startup(function(use)
 	use "norcalli/nvim-colorizer.lua"
 	use "Mofiqul/dracula.nvim"
 
-	-- Mason/LSP
+	-- Mason
 	use "williamboman/mason.nvim"
 	use "williamboman/mason-lspconfig.nvim"
 	use "neovim/nvim-lspconfig"
