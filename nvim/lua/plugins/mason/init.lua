d2c46265feeb1bc2b63163122f4f7442c5e7b988
Martin Cation <martincation@protonmail.com>
2023-05-25 01:54:20 +0700

    neovim: mason menu icons

--- nvim/lua/plugins/mason/init.lua
+++ nvim/lua/plugins/mason/init.lua
@@ -1,4 +1,12 @@
-require("mason").setup()
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "", -- E92B
+			package_pending = "", -- E930
+			package_uninstalled = "", -- EA11
+		},
+	},
+})
 
 require("plugins.mason.installer")
 require("plugins.mason.lint")
