21ac1d347cd5f9cfac27b22f3f01209e5ceb8e10
Martin Cation <martincation@protonmail.com>
2023-05-24 10:21:24 +0700

    completion: use lspkind
    Also install the new plugin in packer.

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,8 +1,21 @@
 local cmp = require("cmp")
+local lspkind = require("lspkind")
 
 require("luasnip.loaders.from_vscode").lazy_load() -- VSCode snippets
 
 cmp.setup({
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol_text",
+			menu = ({
+				buffer = "[Buf]",
+				nvim_lsp = "[LSP]",
+				luasnip = "[Snip]",
+				nvim_lua = "[Lua]",
+				latex_symbols = "[Latex]",
+			})
+		}),
+	},
 	mapping = cmp.mapping.preset.insert({
 		["<c-b>"] = cmp.mapping.scroll_docs(-4),
 		["<c-f>"] = cmp.mapping.scroll_docs(4),
