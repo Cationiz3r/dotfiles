a6590ad6fb0da22c9f49bb6def13197e7e361499
Martin Cation <martincation@protonmail.com>
2023-06-18 11:06:27 +0700

    zsh/env: update PATH once

--- zsh/environment/path.zsh
+++ zsh/environment/path.zsh
@@ -1,12 +1,16 @@
 #!/bin/zsh
 
 typeset -U path PATH
-path=(
-	/usr/lib/ccache/bin
-	"$XDG_CONFIG_HOME"/scripts
-	~/.local/bin
-	~/.local/bin-firejail
-	"$CARGO_HOME"/bin
-	/bin
-	$path
-)
+if [ -z "$PATH_SET" ]; then
+	path=(
+		/usr/lib/ccache/bin
+		"$XDG_CONFIG_HOME"/scripts
+		~/.local/bin
+		~/.local/bin-firejail
+		"$CARGO_HOME"/bin
+		/bin
+		$path
+	)
+
+	export PATH_SET=1
+fi
