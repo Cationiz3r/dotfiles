c62927693bcb93b5b7700114d4364ed4e2eb8838
Martin Cation <martincation@protonmail.com>
2023-08-05 01:47:26 +0700

    zsh/interactive: move pacman.zsh here

--- zsh/interactive/specific.zsh
+++ zsh/interactive/specific.zsh
@@ -3,4 +3,5 @@
 [ -r "$ZSPECDIR/.zshrc" ] && . "$ZSPECDIR/.zshrc"
 [ -d "$ZSPECDIR/completions" ] && for i in "$ZSPECDIR/completions"/_*; do
 	. $i; done; unset i
-&>/dev/null which -p pacman && . "$ZDOTDIR/modules/pacman"
+
+&>/dev/null which -p pacman && . "$ZDOTDIR/interactive/specific/pacman.zsh"
