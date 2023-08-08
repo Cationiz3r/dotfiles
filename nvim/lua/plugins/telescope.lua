48dc0d334d7daa96cbe8487a1f81f132fe9ffe1c
Martin Cation <martincation@protonmail.com>
2023-08-09 02:27:04 +0700

    neovim/plugins: telescope fast quit

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,14 @@
+local actions = require("telescope.actions")
+
 require("telescope").setup({
 	defaults = {
 		prompt_prefix = "❯ ",
 		selection_caret = "❯ ",
 		color_devicons = false,
+		mappings = {
+			i = {
+				["<ESC>"] = actions.close
+			},
+		},
 	}
 })
