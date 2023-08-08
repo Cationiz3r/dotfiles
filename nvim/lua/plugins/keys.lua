0fb5ba57783a2f3ca7867d908ad90ef4548fb649
Martin Cation <martincation@protonmail.com>
2023-08-09 02:01:45 +0700

    plugins/keys: refactor to use register()

--- nvim/lua/plugins/keys.lua
+++ nvim/lua/plugins/keys.lua
@@ -1,121 +1,89 @@
 vim.opt.timeout = true
 vim.opt.timeoutlen = 400
 
-local whichkey = require("which-key")
+local register = require("core.keymaps").register
+
 local telescope = require("telescope.builtin")
 local gitsigns = package.loaded.gitsigns
 local illuminate = require("illuminate")
 local snip = require("luasnip")
 local utils = require("core.utils")
 
--- Quick (no leader)
-whichkey.register({
-	-- vim.keymap.set("n", "e", require("core.utils").close_buffer)
-	["<S-Tab>"] = { "<cmd>BufferLineCyclePrev<CR>", "previous buffer" },
-	["<Tab>"] = { "<cmd>BufferLineCycleNext<CR>", "next buffer" },
-	["<a-e>"] = { ":NvimTreeFocus<CR>", "tree" },
-	["<a-z>"] = { "<c-w><", "minus width" },
-	["<a-c>"] = { "<c-w>>", "add width" },
-	q = { utils.close_buffer, "buffer" },
-	s = { "<cmd>:w<CR>", "save buffer" },
-	K = { vim.lsp.buf.hover, "hover" },
+-- Normal mode
+register({
+	["<a-e>"] = ":NvimTreeFocus<CR>",
+
+	["<S-Tab>"] = "<cmd>BufferLineCyclePrev<CR>",
+	["<Tab>"] = "<cmd>BufferLineCycleNext<CR>",
 
 	["["] = {
-		name = "+previous",
-		c = { gitsigns.prev_hunk, "hunk" },
-		r = { illuminate.goto_prev_reference, "reference" },
-		s = { function() snip.jump(-1) end, "selection" },
+		c = gitsigns.prev_hunk,
+		r = illuminate.goto_prev_reference,
+		s = function() snip.jump(-1) end,
 	},
 	["]"] = {
-		name = "+next",
-		c = { gitsigns.next_hunk, "hunk" },
-		r = { illuminate.goto_next_reference, "reference" },
-		s = { function() snip.jump(1) end, "selection" },
+		c = gitsigns.next_hunk,
+		r = illuminate.goto_next_reference,
+		s = function() snip.jump(1) end,
 	},
+
+	["<leader>"] = {
+		f = {
+			S = telescope.lsp_workspace_symbols,
+			d = telescope.live_grep,
+			f = "<cmd>NvimTreeFindFile<CR>",
+			h = telescope.help_tags,
+			q = telescope.quickfix,
+			r = telescope.lsp_references,
+			s = telescope.lsp_document_symbols,
+			w = telescope.grep_string,
+		},
+
+		g = {
+			R = gitsigns.reset_buffer,
+			S = gitsigns.stage_buffer,
+			b = gitsigns.blame_lines,
+			p = gitsigns.preview_hunk,
+			r = gitsigns.reset_hunk,
+			s = gitsigns.stage_hunk,
+			u = gitsigns.undo_stage_hunk,
+		},
+
+		l = {
+			D = vim.lsp.buf.declaration,
+			a = vim.lsp.buf.code_action,
+			d = vim.lsp.buf.definition,
+			i = vim.lsp.buf.implementation,
+			r = vim.lsp.buf.rename,
+			t = vim.lsp.buf.type_definition,
+		},
+
+		o = {
+			b = telescope.buffers,
+			f = telescope.find_files,
+			g = {
+				b = telescope.git_branches,
+				c = telescope.git_commits,
+			},
+			p = "<cmd>Telescope project<CR>",
+			r = telescope.oldfiles,
+		},
+
+		q = {
+			B = "<cmd>BufOnly!<CR>",
+			W = "<cmd>wincmd o<CR>",
+			w = "<cmd>q<CR>",
+		},
+	},
+
+	K = vim.lsp.buf.hover,
+	q = utils.close_buffer,
 })
 
--- Normal mode
-whichkey.register({
-	r = { "<cmd>source %<CR>", "source current" },
-	h = { "<cmd>nohl<CR>", "no highlight" },
-
-	q = {
-		name = "+quit",
-		q = { "<cmd>wqa<CR>", "all" },
-		d = { "<cmd>qa!<CR>", "delete" },
-		w = {"<cmd>q<CR>", "window"},
-		W = {"<cmd>wincmd o<CR>", "all other windows"},
-		b = { utils.close_buffer, "buffer" },
-		B = { "<cmd>BufOnly!<CR>", "all other buffers" },
-		t = { "<cmd>NvimTreeClose<CR>", "tree" }
-	},
-
-	s = {
-		name = "+show",
-		d = {
-			function()
-				require("gitsigns.actions").diffthis()
-				vim.cmd("windo set foldcolumn=0")
-			end,
-			"git diff"
-		},
-		i = { vim.lsp.buf.signature_help, "signature_help" },
-		r = { vim.lsp.buf.references, "references" },
-	},
-
-	f = {
-		name = "+find",
-		f = { "<cmd>NvimTreeFindFile<CR>", "file in tree" },
-		d = { telescope.live_grep, "in directory" },
-		w = { telescope.grep_string, "word" },
-		s = { telescope.lsp_document_symbols, "document symbols" },
-		S = { telescope.lsp_workspace_symbols, "workspace symbols" },
-		q = { telescope.quickfix, "in quickfix list" },
-		h = { telescope.help_tags, "in help" },
-		r = { telescope.lsp_references, "references" },
-	},
-
+-- Visual mode
+register({
 	g = {
-		name = "+git",
-		s = { gitsigns.stage_hunk, "stage hunk" },
-		S = { gitsigns.stage_buffer, "stage buffer" },
-		r = { gitsigns.reset_hunk, "reset hunk" },
-		R = { gitsigns.reset_buffer, "reset buffer" },
-		u = { gitsigns.undo_stage_hunk, "undo" },
-		p = { gitsigns.preview_hunk, "preview" },
-	},
-
-	o = {
-		name = "+open",
-		t = { ":NvimTreeFocus<CR>", "tree" },
-		f = { telescope.find_files, "file" },
-		r = { telescope.oldfiles, "recent" },
-		b = { telescope.buffers, "buffer" },
-		p = { "<cmd>Telescope project<CR>", "project" },
-		gb = { telescope.git_branches, "git branch" },
-		gc = { telescope.git_commits, "git commit" },
-	},
-
-	l = {
-		name = "+lsp",
-		D = { vim.lsp.buf.declaration, "declaration" },
-		d = { vim.lsp.buf.definition, "definition" },
-		i = { vim.lsp.buf.implementation, "implementation" },
-		t = { vim.lsp.buf.type_definition, "type definition" },
-		a = { vim.lsp.buf.code_action, "code action" },
-		r = { vim.lsp.buf.rename, "rename" },
-	},
-}, { prefix = "<leader>" })
-
--- Visual
-whichkey.register({
-	g = {
-		name = "+git",
-		s = { function()
-			gitsigns.stage_hunk(utils.visual_range())
-		end, "stage hunk" },
-		r = { function()
-			gitsigns.reset_hunk(utils.visual_range())
-		end, "stage hunk" },
+		s = function() gitsigns.stage_hunk(utils.visual_range()) end,
+		r = function() gitsigns.reset_hunk(utils.visual_range()) end,
 	},
 }, { prefix = "<leader>", mode = "v" })
