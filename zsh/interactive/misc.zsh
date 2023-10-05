c2d7553f08c23b59dfa3e97a6c8001ae32e8d544
Martin Cation <martincation@protonmail.com>
2023-10-05 19:53:47 +0700

    zsh: add few completions

--- zsh/interactive/misc.zsh
+++ zsh/interactive/misc.zsh
@@ -14,3 +14,21 @@ command_not_found_handler() {
 
 # Disable pausing/resuming terminal
 stty -ixon
+
+# Few completions
+_detach() {
+	local cmd="$words[1]"
+	local cpp='_comp_priv_prefix=( $cmd -n ${(kv)opt_args[(I)-u]} )'
+	_arguments \
+		"(-)1: :{ $cpp; _command_names -e }" \
+		"*:: :{ $cpp; _normal }"
+}
+compdef _detach detach
+
+_x() {
+	&>/dev/null xset q || return
+	compadd \
+		$(cut -d: -f1 /etc/passwd|awk '{print "+" $0}') \
+		$(xhost|tail +2|cut -d: -f3|awk '{print "-" $0}')
+}
+compdef _x x
