be497451c40df0215f5a329b2dcf095b1ac6dc0c
Martin Cation <martincation@protonmail.com>
2023-11-06 21:42:06 +0700

    neovim/plugins: gitsigns signs cursor line highlighting

--- nvim/lua/plugins/editor/gitsigns.lua
+++ nvim/lua/plugins/editor/gitsigns.lua
@@ -3,7 +3,24 @@ require("gitsigns").setup({
 		delete = { text = "" }, -- E92E
 		topdelete = { text = "" }, -- temp
 	},
+	_extmark_signs = false, -- Use vimfn for cursorline highlight
 })
+
+-- Highlight cursorline in signs column
+-- Credit: https://github.com/m-wells
+-- https://github.com/lewis6991/gitsigns.nvim/issues/563
+vim.defer_fn(function()
+	local bg = vim.api.nvim_get_hl(0, { name = "SignColumn", link = false }).bg
+	local cl_bg = vim.api.nvim_get_hl(0, { name = "CursorLineSign", link = false }).bg
+	for _, sign in ipairs(vim.fn.sign_getdefined()) do
+		local hl = vim.api.nvim_get_hl(0, { name = sign.texthl, link = false })
+		local name = sign.texthl
+		vim.api.nvim_set_hl(0, name, { fg = hl.fg, bg = bg })
+		name = name .. "Cul"
+		vim.api.nvim_set_hl(0, name, { fg = hl.fg, bg = cl_bg })
+		vim.fn.sign_define(sign.name, { culhl = name })
+	end
+end, 10)
 
 -- Update nvimtree
 vim.api.nvim_create_autocmd("User", {
