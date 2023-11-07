9a57badbfdbfaae10911d5758ed4760bb877fee6
Martin Cation <martincation@protonmail.com>
2023-11-08 01:07:45 +0700

    nvim/plugins: hide icons when finding todos

--- nvim/lua/plugins/editor/todo.lua
+++ nvim/lua/plugins/editor/todo.lua
@@ -1,3 +1,14 @@
+local no_icon = { icon = "" }
+
 require("todo-comments").setup({
 	signs = false,
+	keywords = {
+		FIX  = no_icon,
+		TODO = no_icon,
+		HACK = no_icon,
+		WARN = no_icon,
+		PERF = no_icon,
+		NOTE = no_icon,
+		TEST = no_icon,
+	}
 })
