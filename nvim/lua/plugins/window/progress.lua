0f8bc4260a723ee62884a06a9e84cfd078e52641
Martin Cation <martincation@protonmail.com>
2023-12-11 13:27:21 +0700

    neovim/plugins: Custom progress message format

--- nvim/lua/plugins/window/progress.lua
+++ nvim/lua/plugins/window/progress.lua
@@ -7,6 +7,31 @@ require('lsp-progress').setup({
 		"", -- EE0A
 		"", -- EE0B
 	},
+	decay = 200,
+	series_format = function(title, message, percentage, done)
+		if type(title) ~= "string" or string.len(title) == 0 then
+			return nil
+		end
+
+		local builder = {}
+		local escaped_title = title:gsub("%%", "%%%%")
+		table.insert(builder, escaped_title)
+
+		if done then
+			table.insert(builder, " ") -- E92B
+		elseif percentage then
+			table.insert(builder, string.format("%.0f%%%%", percentage))
+		end
+
+		if type(message) == "string" and string.len(message) > 0 then
+			local msg = vim.fn.strdisplaywidth(message) > 20
+				and vim.fn.strpart(message, 0, 17) .. "..."
+				or message
+			local escaped_msg = msg:gsub("%%", "%%%%")
+			table.insert(builder, "\"" .. escaped_msg .. "\"")
+		end
+		return table.concat(builder, " ")
+	end,
 	client_format = function(client_name, spinner, series_messages)
 		return #series_messages > 0
 			and (table.concat(series_messages, ", ") .. "  " .. spinner .. " " .. client_name)
